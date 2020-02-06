a5b977cfa95a3e1421b85adc62e0c53e2e907510
Eugenio <eugeniohernandez17@gmail.com>
2020-02-06 11:16:41 -0500

    CI support.

--- src/main/kotlin/com/example/demo/config/SecurityConfig.kt
+++ src/main/kotlin/com/example/demo/config/SecurityConfig.kt
@@ -2,7 +2,6 @@ package com.example.demo.config
 
 import com.example.demo.service.UserService
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity
@@ -16,8 +15,8 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 @EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true)
 class SecurityConfig : WebSecurityConfigurerAdapter() {
 
-    @get:Bean
-    val passwordEncoder: BCryptPasswordEncoder = BCryptPasswordEncoder()
+    @Autowired
+    lateinit var passwordEncoder: BCryptPasswordEncoder
 
     @Autowired
     lateinit var userService: UserService
